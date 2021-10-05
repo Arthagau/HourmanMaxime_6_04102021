83aae5901ca157cef748c2e070343aeda28336d6
Maxime <hourmanma@gmail.com>
2021-10-05 11:50:32 +0200

    Création routes Utilisateur

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
 const app = express();
+const userRoutes = require("./routes/user");
+
+app.use("api/auth", userRoutes);
 
 mongoose
   .connect(
